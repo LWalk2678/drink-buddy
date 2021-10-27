378383027cc3c369087ce6641121a02b9963ed46
Lance Walker <LWalk2678@msn.com>
2021-10-27 15:41:53 -0600

    working on list

--- drink-buddy/src/App.js
+++ drink-buddy/src/App.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from 'react';
 import { Route, Link } from "react-router-dom";
 //import Form from "./components/Form.jsx";
+import RecipeList from "./components/RecipeList.jsx";
+import RecipeInfo from "./components/RecipeInfo.jsx";
 import './App.css';
 
 const API_URL = 'https://api.airtable.com/v0/appfw6fQjC0mW34OF/Table%201?api_key='
@@ -10,7 +12,7 @@ const API_KEY = 'keyt8Rv1pHLbChQYb';
 function App() {
 
     const [recipes, setRecipes] = useState([]);
-    //const [toggleFetch, setToggleFetch] = useState(true);
+    const [toggleFetch, setToggleFetch] = useState(true);
   
   useEffect(() => {
     console.log('Getting Recipes');
@@ -26,13 +28,36 @@ function App() {
   return (
     <div className="App">
       <nav>
-        Home
+      <Link id= 'home' to="/"><h3>Home</h3></Link>
         Search
         Add
       </nav>
+      <div className='main'>
+        <div className='Recipe List'>Recipe List
+          <Route path='/' exact>
+        
+            {recipes.map((indRecipe) => (
+          
+            <RecipeList 
+            key={indRecipe.id}
+            postData={indRecipe}
+            toggleFetch={toggleFetch}
+            setToggleFetch={setToggleFetch}
+            />
+            ))}
+          </Route>
+
+          <Route path='/recipes/:id' exact>
+        <RecipeInfo
+          recipes = {recipes}
+        />
+          </Route>
+          
+        </div>
+      </div>
 
 
-      <footer>
+      <footer className='footer'>
         GitHub Link
         LinkedIn Link
         Delete record
--- drink-buddy/src/components/RecipeInfo.jsx
+++ drink-buddy/src/components/RecipeInfo.jsx
@@ -9,11 +9,14 @@ const Display = ({recipes}) => {
   return (
 
       <div>
-        <h2>{currentRecipe.fields.name}</h2>
+      <h2>{currentRecipe.fields.Name}</h2>
+      <img src={currentRecipe.fields.image} alt={currentRecipe.fields.Name} className="drinkPhoto"/>
+        <div>
           <ul>
             <li>{currentRecipe.fields.measurement1} {currentRecipe.fields.ingredient1}</li>
             <li>{currentRecipe.fields.ingredient2}</li>
           </ul>
+        </div>
         <p>Instructions:{currentRecipe.fields.instructions}</p>
       </div>
       
